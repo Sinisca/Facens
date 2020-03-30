86d75fb34130e8303d78143ccc0306ffa7a0e805
Pedro Siniscalchi <ppaes94_ps@hotmail.com>
2020-03-30 01:28:09 -0300

    EX1_APLP

--- EX1_APLP.py
+++ EX1_APLP.py
@@ -1,8 +1,8 @@
-    a = int(input('Digite um número: '))
-    print(type(a))
+    a = int(input('Digite um número: ')) #Inserção da variável feita pelo usuário
+    print(type(a)) ##Built-in type mostra qual tipo de item é a variável
 
     def prime_number(a): #Testar se é inteiro
-        if type(a) == int:
+        if type(a) == int: #Built-in type compara se a variável é inteira
             return ("O número {} é um Prime Number!".format(a))
         else:
             return ("Not a Prime Number!")
@@ -13,19 +13,19 @@
         else:
             return ('Invalid Argument')
 
-    def teste_primo(a):
+    def teste_primo(a): #Testar se o número é primo
         if a % 1:
             return ('Prime Number')
         else:
             return ('Not a Prime Number')
 
-    def teste_limite(a):
-        if a < 5.5*10**7:
-            return ('Ok')
+    def teste_limite(a): #Testar se o número encontra-se dentro do limite
+        if a < 5.5*10**7: #Comparação do limite proposto pelo exercício
+            return ('Ok') #Coloquei qualquer amostra só pra não ficar vazio
         else:
             return ('Argument Out of range')
 
-    print(prime_number(a))
-    print(teste_zero(a))
-    print(teste_primo(a))
-    print(teste_limite(a))
+    print(prime_number(a)) #Mostrar valor da função
+    print(teste_zero(a)) #Mostrar valor da função
+    print(teste_primo(a)) #Mostrar valor da função
+    print(teste_limite(a)) #Mostrar valor da função
