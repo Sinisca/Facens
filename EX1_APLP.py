91eac4f31c522f15b8b7f88cea4356ec56019c45
Pedro Siniscalchi <ppaes94_ps@hotmail.com>
2020-03-30 01:24:48 -0300

    EX1_APLP

--- EX1_APLP.py
+++ EX1_APLP.py
@@ -1,27 +1,31 @@
-x = input('Digite um número: ')
-print('O valor inserido foi: {}'.format(x))
+    a = int(input('Digite um número: '))
+    print(type(a))
 
+    def prime_number(a): #Testar se é inteiro
+        if type(a) == int:
+            return ("O número {} é um Prime Number!".format(a))
+        else:
+            return ("Not a Prime Number!")
 
-def prime_number():  # Testar limite do número
-    if x < 5.5 * 10 ** 7:
-        print('Argument out of range')
+    def teste_zero(a): #Testar se o número é maior que zero
+        if (a <= 0):
+            return ('O número {} é maior que zero'.format(a))
+        else:
+            return ('Invalid Argument')
 
+    def teste_primo(a):
+        if a % 1:
+            return ('Prime Number')
+        else:
+            return ('Not a Prime Number')
 
-def teste_primo(x):  # Testar se o número é primo
-    if x % 2 == 1:
-        print("Prime Number!")
-    else:
-        print("Not a Prime Number!")
+    def teste_limite(a):
+        if a < 5.5*10**7:
+            return ('Ok')
+        else:
+            return ('Argument Out of range')
 
-
-def teste_PIZ(x):  # Testar se é positivo, inteiro e maior que zero
-
-    if x is int:
-        print('O numero {} é positivo e maior que zero')
-    else:
-        print('Invalid Argument')
-
-
-print(prime_number(x))
-print(teste_PIZ(x))
-print(teste_primo(x))
+    print(prime_number(a))
+    print(teste_zero(a))
+    print(teste_primo(a))
+    print(teste_limite(a))
