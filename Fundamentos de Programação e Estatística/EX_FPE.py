cdd243ef8d3ef0391d08e30717f4a3777a9e841c
Pedro Paes Siniscalchi <pedrosinisca@outlook.com.br>
2020-04-07 01:51:29 -0300

    EX_FPE

--- Fundamentos de Programação e Estatística/EX_FPE.py
+++ Fundamentos de Programação e Estatística/EX_FPE.py
@@ -1,13 +1,28 @@
 import numpy as np
+import pandas as pd
+import statistics as st
 
 lista = []
 
-def list(lista):
-    x = int(input('Insira a quantidade de elementos da lista 1: '))                     #Inserção dos elementos das lista 1 pelo usuário
+def vetor(lista):
 
-    for i in range(0, x):                                                               #Especificando a quantidade de elementos da lista começando pelo índice 0
-        elementos = int(input('Elemento inserido no índice {}: '.format(i)))            #Inserção dos elementos e mostrando qual índice
-        lista.append(elementos)                                                         #Adiciona um elemento na ultima posição da lista
-    return ('A lista criada é {} '.format(lista))
+    print('\nREGRAS NA FORMAÇÃO DA LISTA:\n'
+          '1.Um dos valores tem que estar repetido\n'
+          '2.O conjunto de dados deve apresentar, pelo menos, 6 valores únicos.\n'
+          '3.NÃO É PERMITIDO escolher todos os números iguais.\n'
+          '4.A LISTA POSSUI APENAS 12 ELEMENTOS\n')
 
-print('A média dos elementos é: ', np.mean(lista))                                      #Média dos Elementos
+    for i in range(0, 12):
+        elementos = int(input('Elemento inserido no índice {}: '.format(i)))    #Inserção dos elementos e mostrando qual índice
+        lista.append(elementos)                                                #Adiciona um elemento na ultima posição da lista
+    return(lista)
+
+print('A sua lista é: ', vetor(lista))
+print('A média dos índices é: {:.4f} '.format(np.mean(vetor(lista))))
+print('A moda é: {:.4f} '.format(st.mode(lista)))
+print('A Mediana é: {:.4f} '.format(st.median(vetor(lista))))
+print('A variância amostral é: {:.4f}'.format(st.pvariance(vetor(lista))))
+print('O desvio padrão amostral é: {:.4f}'.format(st.stdev(vetor(lista))))
+print('O coeficiente de variação é: {:.4f}'.format(st.variance(vetor(lista))))
+
+
